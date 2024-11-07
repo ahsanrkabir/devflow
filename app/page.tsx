0c93506bb9bc0f8752a5a16af50faf8aa416cd64
Ahsan Kabir <ahsanrifat001@gmail.com>
2024-11-08 02:10:08 +0600

    Set up Light & Dark Theme, Shadcn, Navbar (pt. 2)

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ const Home = () => {
   return (
     <div>
       <h1 className="h1-bold">Welcome to DevFlow</h1>
-      <h1 className="h1-bold font-space-grotesk">Welcome to DevFlow</h1>
     </div>
   );
 };
