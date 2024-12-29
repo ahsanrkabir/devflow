b3a19c1f9707aa160d1d4435513b6b7ac41546a1
ahsanrkabir <ahsanrifat001@gamil.com>
2024-12-29 17:20:54 +0600

    Implement Action Handler

--- auth.ts
+++ auth.ts
@@ -3,7 +3,7 @@ import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 
 import { IAccountDoc } from "./database/account.model";
-import { api } from "./lib/handlers/api";
+import { api } from "./lib/api";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [GitHub, Google],
