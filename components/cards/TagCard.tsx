ce2a439127828cf20b72368edd2a498f0202b4e4
ahsanrkabir <ahsanrifat001@gamil.com>
2025-01-08 23:46:41 +0600

    Implement Tag Questions Action

--- components/cards/TagCard.tsx
+++ components/cards/TagCard.tsx
@@ -73,29 +73,31 @@ const TagCard = ({
     );
   }
 
-  return (
-    <Link href={ROUTES.TAG(_id)} className="shadow-light100_darknone">
-      <article className="background-light900_dark200 light-border flex w-full flex-col rounded-2xl border px-8 py-10 sm:w-[260px]">
-        <div className="flex items-center justify-between gap-3">
-          <div className="background-light800_dark400 w-fit rounded-sm px-5 py-1.5">
-            <p className="paragraph-semibold text-dark300_light900">{name}</p>
+  if (questions && questions > 0) {
+    return (
+      <Link href={ROUTES.TAG(_id)} className="shadow-light100_darknone">
+        <article className="background-light900_dark200 light-border flex w-full flex-col rounded-2xl border px-8 py-10 sm:w-[260px]">
+          <div className="flex items-center justify-between gap-3">
+            <div className="background-light800_dark400 w-fit rounded-sm px-5 py-1.5">
+              <p className="paragraph-semibold text-dark300_light900">{name}</p>
+            </div>
+            <i className={cn(iconClass, "text-2xl")} aria-hidden={true} />
           </div>
-          <i className={cn(iconClass, "text-2xl")} aria-hidden={true} />
-        </div>
 
-        <p className="small-regular text-dark500_light700 mt-5 line-clamp-3 w-full">
-          {iconDescription}
-        </p>
+          <p className="small-regular text-dark500_light700 mt-5 line-clamp-3 w-full">
+            {iconDescription}
+          </p>
 
-        <p className="small-medium text-dark400_light500 mt-3.5">
-          <span className="body-semibold primary-text-gradient mr-2.5">
-            {questions}+
-          </span>
-          Questions
-        </p>
-      </article>
-    </Link>
-  );
+          <p className="small-medium text-dark400_light500 mt-3.5">
+            <span className="body-semibold primary-text-gradient mr-2.5">
+              {questions}+
+            </span>
+            Questions
+          </p>
+        </article>
+      </Link>
+    );
+  }
 };
 
 export default TagCard;
