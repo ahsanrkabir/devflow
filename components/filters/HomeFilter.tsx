66f0abad42ad9d7ca6dee31b4d828c0fc99e6d63
ahsanrkabir <ahsanrifat001@gamil.com>
2025-01-24 02:59:09 +0600

    Implement Question Details Page

--- components/filters/HomeFilter.tsx
+++ components/filters/HomeFilter.tsx
@@ -9,12 +9,12 @@ import { cn } from "@/lib/utils";
 import { Button } from "../ui/button";
 
 const filters = [
-  { name: "React", value: "react" },
-  { name: "JavaScript", value: "javascript" },
-  // { name: "Newest", value: "newest" },
-  // { name: "Popular", value: "popular" },
-  // { name: "Unanswered", value: "unanswered" },
-  // { name: "Recommended", value: "recommended" },
+  // { name: "React", value: "react" },
+  // { name: "JavaScript", value: "javascript" },
+  { name: "Newest", value: "newest" },
+  { name: "Popular", value: "popular" },
+  { name: "Unanswered", value: "unanswered" },
+  { name: "Recommended", value: "recommended" },
 ];
 
 const HomeFilter = () => {
