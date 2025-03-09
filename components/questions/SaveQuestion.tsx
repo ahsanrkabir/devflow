d29a70116bb88484055d0bb5b3974999be6f91a7
ahsanrkabir <ahsanrifat001@gamil.com>
2025-03-10 02:34:24 +0600

    Show User Has Voted Question

--- components/questions/SaveQuestion.tsx
+++ components/questions/SaveQuestion.tsx
@@ -2,17 +2,26 @@
 
 import Image from "next/image";
 import { useSession } from "next-auth/react";
-import { useState } from "react";
+import { use, useState } from "react";
 
 import { toast } from "@/hooks/use-toast";
 import { toggleSaveQuestion } from "@/lib/actions/collection.action";
 
-const SaveQuestion = ({ questionId }: { questionId: string }) => {
+const SaveQuestion = ({
+  questionId,
+  hasSavedQuestionPromise,
+}: {
+  questionId: string;
+  hasSavedQuestionPromise: Promise<ActionResponse<{ saved: boolean }>>;
+}) => {
   const session = useSession();
   const userId = session?.data?.user?.id;
 
+  const { data } = use(hasSavedQuestionPromise);
+
+  const { saved: hasSaved } = data || {};
+
   const [isLoading, setIsLoading] = useState(false);
-  const hasSaved = false;
 
   const handleSave = async () => {
     if (isLoading) return;
