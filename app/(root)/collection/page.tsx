58b0039fb13e69618d382d021218ed353332210b
ahsanrkabir <ahsanrifat001@gamil.com>
2025-03-16 04:13:22 +0600

    FIX: Implement PipelineAggregration to Get Questions from Collection Model

--- app/(root)/collection/page.tsx
+++ app/(root)/collection/page.tsx
@@ -2,8 +2,10 @@ import Link from "next/link";
 
 import QuestionCard from "@/components/cards/QuestionCard";
 import DataRenderer from "@/components/DataRenderer";
+import CommonFilter from "@/components/filters/CommonFilter";
 import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
+import { CollectionFilters } from "@/constants/filters";
 import ROUTES from "@/constants/routes";
 import { EMPTY_QUESTION } from "@/constants/states";
 import { getSavedQuestions } from "@/lib/actions/collection.action";
@@ -36,12 +38,17 @@ const Collections = async ({ searchParams }: SearchParams) => {
           <Link href={ROUTES.ASK_QUESTION}>Ask A Question</Link>
         </Button>
       </section>
-      <section className="mt-11">
+      <section className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
         <LocalSearch
           imgSrc="/icons/search.svg"
           placeholder="Search for questions..."
           otherClasses="flex-1"
           route={ROUTES.COLLECTION}
+        />
+
+        <CommonFilter
+          filters={CollectionFilters}
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
         />
       </section>
 
